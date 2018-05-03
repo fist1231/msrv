f7f6ec70dddf79706ab0e232d3a94306bfeeaf26
tsa <stislin@gmail.com>
2018-05-02 23:09:25 -0400

    gateway api schema stitching-1

--- services/nress/gateway/index.js
+++ services/nress/gateway/index.js
@@ -2,8 +2,14 @@ import Rx from 'rxjs';
 import cors from 'cors';
 import express from 'express';
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
-import { makeExecutableSchema } from 'graphql-tools';
+import { makeExecutableSchema, mergeSchemas } from 'graphql-tools';
 import config from './config/config.json'
+
+
+const fetch = require('node-fetch')
+const { makeRemoteExecutableSchema, introspectSchema } = require('graphql-tools')
+const { createHttpLink } = require('apollo-link-http')
+
 
 export const app = express(),
   port = process.env.PORT || 3330,
@@ -13,8 +19,8 @@ app.use(cors());
 
 
 // GraphQL
-import schema  from './api/schema/schema';
-import resolvers from './api/schema/resolvers';
+// import schema  from './api/schema/schema';
+// import resolvers from './api/schema/resolvers';
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -23,19 +29,69 @@ var routes = require('./api/routes/gatewayRoutes'); //importing route
 routes(app); //register the route
 
 
-const sch = makeExecutableSchema({
-  typeDefs: [schema],
-  //rootResolvers
-  resolvers,
-});
-
-// The GraphQL endpoint
-app.use('/graphql', bodyParser.json(), graphqlExpress({ schema: sch }));
-
-// GraphiQL, a visual editor for queries
-app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+// const sch = makeExecutableSchema({
+//   typeDefs: [schema],
+//   //rootResolvers
+//   resolvers,
+// });
 
 
-app.listen(port, () => {
-  console.log('Nress Gateway REST/GraphQL API server started on: ' + port);
-});
+async function run() {
+
+  const makeSolicitationsServiceLink = () => createHttpLink({
+    uri: `${config.solicitations_address}/graphql`,
+    fetch
+  })
+
+  // 2. Retrieve schema definition of the underlying GraphQL API
+  const solicitationsServiceSchemaDefinition = await introspectSchema(makeSolicitationsServiceLink())
+
+  // 3. Create the executable schema based on schema definition and Apollo Link
+  const solicitationsServiceExecutableSchema = makeRemoteExecutableSchema({
+    schema: solicitationsServiceSchemaDefinition,
+    link: makeSolicitationsServiceLink()
+  })
+
+
+  const makeReviewsServiceLink = () => createHttpLink({
+    uri: `${config.reviews_address}/graphql`,
+    fetch
+  })
+
+  // 2. Retrieve schema definition of the underlying GraphQL API
+  const reviewsServiceSchemaDefinition = await introspectSchema(makeReviewsServiceLink())
+
+  // 3. Create the executable schema based on schema definition and Apollo Link
+  const reviewsServiceExecutableSchema = makeRemoteExecutableSchema({
+    schema: reviewsServiceSchemaDefinition,
+    link: makeReviewsServiceLink()
+  })
+
+
+  const schema = mergeSchemas({
+    schemas: [
+      solicitationsServiceExecutableSchema,
+      reviewsServiceExecutableSchema
+    ],
+  });
+
+
+  // The GraphQL endpoint
+  // app.use('/graphql', bodyParser.json(), graphqlExpress({ schema: sch }));
+  app.use('/graphql', bodyParser.json(), graphqlExpress({ schema: schema  }));
+
+  // GraphiQL, a visual editor for queries
+  app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+
+
+  app.listen(port, () => {
+    console.log('Nress Gateway REST/GraphQL API server started on: ' + port);
+  });
+
+}
+
+try {
+	run();
+} catch (e) {
+	console.log(e, e.message, e.stack);
+}
